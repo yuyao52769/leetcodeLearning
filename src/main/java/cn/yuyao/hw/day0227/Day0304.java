56aeaede2a985670384051e9dfa8dbcc9b5f9c8c
y29249 <yu.yaoA@hac.com>
2024-03-04 18:17:08 +0800

    push

---
+++ src/main/java/cn/yuyao/hw/day0227/Day0304.java
@@ -0,0 +1,18 @@
+package cn.yuyao.hw.day0227;
+
+public class Day0304 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 整数对最小和
+     */
+    public int test1(String str) {
+        if (str.length() == 0) return 0;
+        if (str.length() == 1) return str.charAt(0) == '0' ? 1 : 0;
+        int count = 0;
+        return count;
+    }
+}
